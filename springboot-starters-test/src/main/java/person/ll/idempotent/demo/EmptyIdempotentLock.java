993b81e323e645e43d73012cc513e1428c7a8b4e
wll <wull@juwan.group>
2020-05-26 13:29:59 +0800

    幂等

--- springboot-starters-test/src/main/java/person/ll/idempotent/demo/EmptyIdempotentLock.java
+++ springboot-starters-test/src/main/java/person/ll/idempotent/demo/EmptyIdempotentLock.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class EmptyIdempotentLock extends IdempotentLockSupport {
 
     public EmptyIdempotentLock() {
-        super(LockWay.MYSQL);
+        super(LockWay.CUSTOMIZE);
     }
 
 
--- springboot-starters/idempotent-spring-boot-starter/src/main/java/person/ll/idempotent/spring/boot/starter/enums/LockWay.java
+++ springboot-starters/idempotent-spring-boot-starter/src/main/java/person/ll/idempotent/spring/boot/starter/enums/LockWay.java
@@ -4,5 +4,5 @@ package person.ll.idempotent.spring.boot.starter.enums;
  * 存储介质
  */
 public enum LockWay {
-    REDIS,REDISSION,MYSQL
+    REDIS,REDISSION,CUSTOMIZE
 }
